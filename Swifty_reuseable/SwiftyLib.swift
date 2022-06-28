d44a87b82328fd2072d4525bac9f30ca2c90df41
amit-chowdhury-bjit <89369320+amit-chowdhury-bjit@users.noreply.github.com>
2022-06-28 17:00:09 +0600

    SwiftyLib added

---
+++ Swifty_reuseable/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  StringExtension.swift
+//  Swifty_reuseable
+//
+//  Created by BJIT on 28/6/22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
